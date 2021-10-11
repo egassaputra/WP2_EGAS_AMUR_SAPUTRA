d087533356e9ad3c2ad3c8e7026f0a5c0ed35e09
egassaputra <egasamursaputra36@gmail.com>
2021-10-11 22:49:08 +0700

    penambahan isi contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Egas Amur Saputra
+ Saya tinggal di daerah Koja Jakarta Utara
+ olahraga yang saya sukai adalah
+ Futsal";
+ }
+}
